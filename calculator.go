a7b5e1f5c056271b400041c58af1b42295a4aee8
Enes Kerem AYDIN <46195766+EnesKeremAYDIN@users.noreply.github.com>
2024-08-17 01:13:30 +0300

    Create calculator.go

---
+++ calculator.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+    var height int
+    fmt.Print("Input your height (cm): ")
+    fmt.Scanf("%d", &height)
+    fmt.Printf("Your height is %d cm\n", height)
+}
