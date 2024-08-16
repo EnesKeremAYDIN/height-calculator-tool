dc9379afbf6378f740aeb11bab60ab831ab61f2f
Enes Kerem AYDIN <46195766+EnesKeremAYDIN@users.noreply.github.com>
2024-08-17 01:13:25 +0300

    Create calculator.c

---
+++ calculator.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int height;
+    printf("Input your height (cm): ");
+    scanf("%d", &height);
+    printf("Your height is %d cm\n", height);
+    return 0;
+}
