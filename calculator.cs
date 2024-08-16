037baeb1f489b1ba0efad82c5ad4f84f28e0fd9d
Enes Kerem AYDIN <46195766+EnesKeremAYDIN@users.noreply.github.com>
2024-08-17 01:13:27 +0300

    Create calculator.cs

---
+++ calculator.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Input your height (cm): ");
+        string input = Console.ReadLine();
+        Console.WriteLine("Your height is " + input + " cm");
+    }
+}
