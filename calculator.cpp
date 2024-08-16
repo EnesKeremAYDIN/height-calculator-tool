fe4d14edec269a0f3156a085ebc025a81848a918
Enes Kerem AYDIN <46195766+EnesKeremAYDIN@users.noreply.github.com>
2024-08-17 01:13:28 +0300

    Create calculator.cpp

---
+++ calculator.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    int height;
+    std::cout << "Input your height (cm): ";
+    std::cin >> height;
+    std::cout << "Your height is " << height << " cm" << std::endl;
+    return 0;
+}
